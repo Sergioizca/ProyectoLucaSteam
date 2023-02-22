212e6fbf32b641a306aef35edfb609e4f5b24a61
Sergio Izquierdo <sergioizca1996@gmail.com>
2023-02-22 13:17:12 +0100

    Añadido entorno de Pruebas

--- mostrar_lista_juegos.py
+++ mostrar_lista_juegos.py
@@ -1,5 +1,17 @@
 # Creamos una función mostrar_lista_juegos() que devuelve el resultado de la lista de diccionarios
 
+# ***PRUEBAS*** from convertir_csv_diccionario import *
 
 def mostrar_lista_juegos(lista):
     print(lista)
+
+"""
+# ***PRUEBAS***
+
+def main():
+    mostrar_lista_juegos(convertir_csv_diccionarios("prueba.csv"))
+
+
+main()
+
+"""
