fa9de6917459896927835b8e1780c0c382983869
Steeven <steevendaniel4@gmail.com>
2023-02-22 18:29:07 +0100

    Correccion correcta del menu

--- menu.py
+++ menu.py
@@ -7,6 +7,7 @@ from modificar_juego import *
 
 def menu(lista):
     seguir=True
+    lista=convertir_csv_diccionarios("vgsales.csv")
     while seguir:
         print(" **** MENU **** \n **** 1.Añadir **** \n **** 2.Borrar **** \n **** 3.Modificar **** \n **** 4.Listar **** \n **** 5.Salir **** \n ***************")
         valor=int(input("Selecciona una opción: "))
@@ -20,7 +21,7 @@ def menu(lista):
                     borrar_de_dic(lista)
                 case 3 | None:
                     print("Modificar")
-                    # modificar_juego(obtener_cambio())
+                    modificar_juego(encontrar(lista), obtener_cambio())
                 case 4 | None:
                     print("Listar")
                     mostrar_lista_juegos(lista)
@@ -29,31 +30,6 @@ def menu(lista):
                     seguir=False
         else:
             print("----------Selecciona una opción correcta----------")
-
-seguir=True
-lista=convertir_csv_diccionarios("vgsales.csv")
-while seguir:
-    print(" **** MENU **** \n **** 1.Añadir **** \n **** 2.Borrar **** \n **** 3.Modificar **** \n **** 4.Listar **** \n **** 5.Salir **** \n ***************")
-    valor=int(input("Selecciona una opción: "))
-    if (valor>0 and valor<=5):
-        match valor:
-            case 1 | None:
-                # agregar_juego()
-                print("Añadir")
-            case 2 | None:
-                print("Eliminar")
-                borrar_de_dic(lista)
-            case 3 | None:
-                print("Modificar")
-                modificar_juego(encontrar(lista), obtener_cambio())
-            case 4 | None:
-                print("Listar")
-                mostrar_lista_juegos(lista)
-            case 5 | None:
-                print("Has salido correctamente")
-                seguir=False
-    else:
-        print("----------Selecciona una opción correcta----------")
 if __name__ == "__main__":
     def main():
         lista=convertir_csv_diccionarios("vgsales.csv")
