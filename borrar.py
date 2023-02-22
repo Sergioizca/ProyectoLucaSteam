d949f0285834db7ad34eee986e69733dabdf7657
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-22 10:59:19 +0100

    prueba

--- borrar.py
+++ borrar.py
@@ -1,2 +1,17 @@
 import csv
 
+juegos = {1:2, 2:1, 5:2}
+
+def borrar(juegos):
+    a = True
+    while True:
+        nombre_borrar = int(input("Inserta el nombre del juego que deseas borrar"))
+        if nombre_borrar in juegos:
+            del juegos[nombre_borrar]
+            print("Se ha borrado la entrada seleccionada")
+            a = False
+        else:
+            print("Ese videojuego no consta en nuestro archivo. Vuelve a intentarlo")
+            
+borrar(juegos)
+print(juegos) #Esto se quita despues
