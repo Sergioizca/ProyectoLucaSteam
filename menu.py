c14d43119559c2fae5eaa9ac892df67effd1229e
Sergio Izquierdo <sergioizca1996@gmail.com>
2023-02-22 18:15:32 +0100

    Añadidas pruebas unitarias mostrar_lista_juegos

--- menu.py
+++ menu.py
@@ -3,30 +3,35 @@ from convertir_csv_diccionario import *
 from borrar import *
 from mostrar_lista_juegos import *
 
+def menu(lista):
+    seguir=True
+    while seguir:
+        print(" **** MENU **** \n **** 1.Añadir **** \n **** 2.Borrar **** \n **** 3.Modificar **** \n **** 4.Listar **** \n **** 5.Salir **** \n ***************")
+        valor=int(input("Selecciona una opción: "))
+        if (valor>0 and valor<=5):
+            match valor:
+                case 1 | None:
+                    # agregar_juego()
+                    print("Añadir")
+                case 2 | None:
+                    print("Eliminar")
+                    borrar_de_dic(lista)
+                case 3 | None:
+                    print("Modificar")
+                    # modificar_juego(obtener_cambio())
+                case 4 | None:
+                    print("Listar")
+                    mostrar_lista_juegos(lista)
+                case 5 | None:
+                    print("Has salido correctamente")
+                    seguir=False
+        else:
+            print("----------Selecciona una opción correcta----------")
 
-seguir=True
-lista=convertir_csv_diccionarios("vgsales.csv")
-while seguir:
-    print(" **** MENU **** \n **** 1.Añadir **** \n **** 2.Borrar **** \n **** 3.Modificar **** \n **** 4.Listar **** \n **** 5.Salir **** \n ***************")
-    valor=int(input("Selecciona una opción: "))
-    if (valor>0 and valor<=5):
-        match valor:
-            case 1 | None:
-                # agregar_juego()
-                print("Añadir")
-            case 2 | None:
-                print("Eliminar")
-                borrar_de_dic(lista)
-            case 3 | None:
-                print("Modificar")
-                # modificar_juego(obtener_cambio())
-            case 4 | None:
-                print("Listar")
-                mostrar_lista_juegos(lista)
-            case 5 | None:
-                print("Has salido correctamente")
-                seguir=False
-    else:
-        print("----------Selecciona una opción correcta----------")
+if __name__ == "__main__":
+    def main():
+        lista=convertir_csv_diccionarios("vgsales.csv")
+        menu(lista)
+    main()
 
 # Pdte ampliar el menú (si fuera necesario), try-except.
---
+++ pruebasunitarias.py
@@ -0,0 +1,41 @@
+from mostrar_lista_juegos import *
+from convertir_csv_diccionario import *
+from menu import *
+
+lista = convertir_csv_diccionarios("vgsales.csv")
+
+#Prueba unitaria01 mostrar_lista_juegos
+
+def prueba_mostrar_lista_juegos(lista):
+    check1 = len(lista)
+    check2 = len(mostrar_lista_juegos(lista))
+    ok = False
+    if(check1==check2):
+        ok=True
+        print("Carga de datos correcta")
+    else:
+        ok=False
+        print("Error en la carga de datos")
+    return ok
+
+#Prueba unitaria02 mostrar_lista_juegos
+def prueba_mostrar_lista_juegos2(lista):
+    check1 = len(lista)
+    check2 = len(mostrar_lista_juegos(lista))
+    assert check1==check2, "Carga de datos incorrecta"
+    print("Carga de datos correcta")
+
+#Prueba unitaria01 menu
+def prueba_menu(lista):
+    pass
+
+#Prueba unitaria02 menu
+def prueba_menu2(lista):
+    pass
+
+def main():
+    prueba_mostrar_lista_juegos(lista)
+    prueba_mostrar_lista_juegos2(lista)
+
+
+main()
