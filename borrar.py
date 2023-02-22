e62f6f795e624e49cbffbf53b04187211053576c
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-22 13:53:33 +0100

    pruebas en borrar

--- borrar.py
+++ borrar.py
@@ -1,21 +1,31 @@
 import csv
+import os
 from convertir_csv_diccionario import *
 
 # Introducir bucle que pregunte si quiere salir con teclado
 # o si quiere continuar eliminando otro juego
 
+
+def pregunta_continuar(mensaje):
+    respuesta = input(mensaje).lower()
+    if respuesta == "n":
+        return False
+    return True
+
+
 def borrar(juegos):
-    a = True
-    while True:
+    continuar = True
+    while continuar:
         nombre_borrar = input(
             "Inserta el nombre del juego que deseas borrar: \n")
         for i in juegos:
             if i["nombre"] == nombre_borrar and a:
                 juegos.remove(i)
                 print("Se ha borrado la entrada seleccionada")
-                a = False
+                continuar = pregunta_continuar("¿Quieres eliminar otra entrada? s/n:\n")
             else:
-                print("Ese videojuego no consta en nuestro archivo. Vuelve a intentarlo")
+                print("Ese videojuego no consta en nuestro archivo")
+                continuar = pregunta_continuar("¿Quieres eliminar otra entrada? s/n:\n")
 
 
 def main():
