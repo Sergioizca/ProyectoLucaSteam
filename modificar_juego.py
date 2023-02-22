d31885e05e575817287bb8ba4da98056d846a127
Steeven <steevendaniel4@gmail.com>
2023-02-22 18:34:53 +0100

    Eliminacion print de lista previa

--- modificar_juego.py
+++ modificar_juego.py
@@ -3,7 +3,6 @@ import os
 from convertir_csv_diccionario import *
 from obtener_cambio import *
 def encontrar(juegos):
-    print (juegos)
     nombre_encontrar = input(
             "Inserta el nombre del juego que deseas encontrar: \n")    
     for juego in juegos:
