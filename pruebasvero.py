be47c88f034ccbd496fd4e4d3ddd1b070c3a424e
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-22 18:24:12 +0100

    aa

--- pruebasvero.py
+++ pruebasvero.py
@@ -1,12 +1,10 @@
 from borrar import *
 from convertir_csv_diccionario import *
+from menu import *
 
 # Prueba unitaria para borrar.py
 # Comprueba que ocurre si el csv no se ha leído correctamente
 lista = convertir_csv_diccionarios("vgsales.csv")
-def prueba_borrar_de_dic(lista):
-    print(len(lista))
-    
-prueba_borrar_de_dic()
 
+def prueba_borrar_de_dic():
     
