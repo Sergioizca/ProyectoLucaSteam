8c1dbbe24cbf2056fe7c8d59ae0cba8c91715a4e
Sergio Izquierdo <sergioizca1996@gmail.com>
2023-02-22 10:03:01 +0100

    Añadimos la estructura del Menú

---
+++ menu.py
@@ -0,0 +1,20 @@
+#menu
+print (" **** MENU **** \n **** 1.Añadir **** \n **** 2.Borrar **** \n **** 3.Modificar **** \n **** 4.Listar **** \n **** 5.Salir **** \n ***************")
+valor = int(input())
+
+match valor:
+    case 1 | None:
+        #agregar_juego()
+        print("Añadir")
+    case 2 | None:
+        print("Borrar")
+        #borrar_juego()
+    case 3 | None:
+        print("Modificar")
+        #modificar_juego(obtener_cambio()) 
+    case 4 | None:
+        print("Listar")
+        #mostrar_lista_juegos()
+    case 5 | None:
+        print("Salir")
+
