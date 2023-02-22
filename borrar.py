94fdc2cf2e564bfeba0e30ee4b6d927d75a4e61c
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-22 11:07:47 +0100

    cambios en borrar

--- borrar.py
+++ borrar.py
@@ -1,17 +1,22 @@
 import csv
+from convertir_csv_diccionario import *
 
 
 
 def borrar(juegos):
     a = True
     while True:
-        nombre_borrar = int(input("Inserta el nombre del juego que deseas borrar"))
+        nombre_borrar = input("Inserta el nombre del juego que deseas borrar: \n")
         if nombre_borrar in juegos:
             del juegos[nombre_borrar]
             print("Se ha borrado la entrada seleccionada")
             a = False
         else:
             print("Ese videojuego no consta en nuestro archivo. Vuelve a intentarlo")
-            
-borrar(juegos)
-print(juegos) #Esto se quita despues
+
+
+
+def main():
+    borrar(convertir_csv_diccionarios("prueba.csv"))
+    
+main()
