fc334b12de782ae76d0c951c632c3b8c42e1389d
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-23 12:49:01 +0100

    asd

--- listar_juegos_genero.py
+++ listar_juegos_genero.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 #Esta función busca el archivo y ordena los juegos por género
 def listar_juegos_genero(archivo):
-
+    genero = input("¿Qué género desea buscar?:\n")
     df = pd.read_csv(archivo)
-    df = df.sort_values[df['Genre']]
-    print(df)
+    juegos_por_genero =  df.loc[df['Genre'].isin([genero, 'N/A', ""])]
+    
+    print(f"Los juegos de {genero} son:")
+    print(juegos_por_genero)
 
 
-# buscar_siglo_veinte("vgsales.csv")
+listar_juegos_genero("vgsales.csv")
