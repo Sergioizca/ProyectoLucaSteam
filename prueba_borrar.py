b4884b62e8953b1bb44b7d9bd14d07e8232343b2
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-23 18:25:35 +0100

    añado prueba unitaria genero media que funciona correctamente aleluya

--- prueba_borrar.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-def esta_juego_presente(juegos, nombre)
-    nombre_a_borrar = juegos[0]["nombre"] # pillamos el primer juego
-    borrar_juego(juegos, nombre_a_borrar)
-    assert(not esta_juego_presente(juegos, nombre_a_borrar)
