eb6abe43c3b190c8364968e032560b7d6f87329e
Steeven <steevendaniel4@gmail.com>
2023-02-23 13:28:38 +0100

    Ajustado a PEP8

--- buscar_editores.py
+++ buscar_editores.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+
 def buscar_editores(archivo):
-    editor= input("Indique el editor del que desea buscar sus publicaciones: ")
+    editor = input(
+        "Indique el editor del que desea buscar sus publicaciones: ")
     datos = pd.read_csv(archivo)
-    juegos_editor =  datos.loc[datos['Publisher'].isin([editor, 'N/A', ""])]
-    
+    juegos_editor = datos.loc[datos['Publisher'].isin([editor, 'N/A', ""])]
+
     print(f"Los juegos de {editor} son:")
     print(juegos_editor)
