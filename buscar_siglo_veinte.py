140bb50cc7f19e5656323178417c7c52d57a0d7e
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-23 12:24:00 +0100

    cambios

--- buscar_siglo_veinte.py
+++ buscar_siglo_veinte.py
@@ -4,7 +4,6 @@ import pandas as pd
 def buscar_siglo_veinte(archivo):
 
     df = pd.read_csv(archivo)
-    df = pd.read_csv(archivo)
     df = df[df['Year'] < 2000]
     print("Juegos del siglo XX:\n")
     print(df)
