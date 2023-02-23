6a94f2b4363a77b0f03bbddb9caa18d15806eace
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-23 10:53:27 +0100

    cambio menu

--- menu.py
+++ menu.py
@@ -6,33 +6,36 @@ from obtener_cambio import *
 from modificar_juego import *
 from Añadir_juego import *
 
+
 def menu(lista):
-    seguir=True
+    seguir = True
     while seguir:
-        print(" Bienvenido al menú. Teclee el número al que desea acceder: \n\n 1. Añadir videojuego a la lista \n\n 2. Borrar videojuego de la lista \n\n 3. Modificar videojuego de la lista \n\n 4. Ver listado completo de videojuegos \n\n 5. Salir del menú \n ")
-        valor=int(input("Selecciona una opción: "))
-        if (valor>0 and valor<=5):
+        print("\nBienvenido al menú del listado de videojuegos. Teclee el número al que desea acceder: \n\n 1. Añadir videojuego a la lista \n\n 2. Borrar videojuego de la lista \n\n 3. Modificar videojuego de la lista \n\n 4. Ver listado completo de videojuegos \n\n 5. Salir del menú \n ")
+        valor = int(input("Selecciona una opción: "))
+        if (valor > 0 and valor <= 5):
             match valor:
                 case 1 | None:
-                    print("Añadir")
+                    print("Añadir videojuego a la lista")
                     agregar_juego(lista)
                 case 2 | None:
-                    print("Eliminar")
+                    print("Borrar videojuego de la lista")
                     borrar_de_dic(lista)
                 case 3 | None:
-                    print("Modificar")
+                    print("Modificar videojuego de la lista")
                     modificar_juego(encontrar(lista), obtener_cambio())
                 case 4 | None:
                     print("Listar")
                     mostrar_lista_juegos()
                 case 5 | None:
                     print("Has salido correctamente")
-                    seguir=False
+                    seguir = False
         else:
             print("----------Selecciona una opción correcta----------")
+
+
 if __name__ == "__main__":
     def main():
-        lista=convertir_csv_diccionarios("vgsales.csv")
+        lista = convertir_csv_diccionarios("vgsales.csv")
         menu(lista)
     main()
 
