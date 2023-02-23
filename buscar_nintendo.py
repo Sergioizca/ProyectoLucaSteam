89a28c4353dca92eb00b7950b15f7bb23623ff6c
Sergio Izquierdo <sergioizca1996@gmail.com>
2023-02-23 11:34:18 +0100

    Fix errores

--- buscar_nintendo.py
+++ buscar_nintendo.py
@@ -4,7 +4,7 @@ import pandas as pd
 def buscar_nintendo(archivo):
 
     datos = pd.read_csv(archivo)
-    nintendo_games = datos.loc[datos['Publisher'].isin(['Nintendo', 'N/A', ""])]
+    nintendo_games = datos.loc[datos['Publisher'].isin(['Nintendo'])]
     print("Juegos de Nintendo:")
     print(nintendo_games)
 
