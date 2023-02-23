ab3d340151e0ec696f6e24f803c1305bbc27556a
Sergio Izquierdo <sergioizca1996@gmail.com>
2023-02-23 16:45:04 +0100

    Arreglo fallos unitarias

--- pruebasunitarias.py
+++ pruebasunitarias.py
@@ -12,29 +12,29 @@ class TestBuscarNintendo(unittest.TestCase):
         buscar_nintendo(archivo)
 
 
-def load_csv_to_dict_list(csv_file):
+def carga_csv_lista(archivo):
     """Carga un archivo CSV en una lista de diccionarios"""
-    df = pd.read_csv(csv_file)
-    dict_list = df.to_dict('records')
-    return dict_list
+    datos = pd.read_csv(archivo)
+    lista = datos.to_dict('records')
+    return lista
 
 #dado un archivo csv con pandas compruebe que han cargado correctamente en una lista de diccionarios.
 class TestCarga(unittest.TestCase):
 
-    def test_load_csv_to_dict_list(self):
-        dict_list = load_csv_to_dict_list('vgsales.csv')
-        self.assertEqual(len(dict_list), 16598)
-        self.assertEqual(dict_list[0]['Rank'], 1)
-        self.assertEqual(dict_list[0]['Name'], 'Wii Sports')
-        self.assertEqual(dict_list[0]['Platform'], 'Wii')
-        self.assertEqual(dict_list[0]['Year'], 2006)
-        self.assertEqual(dict_list[0]['Genre'], 'Sports')
-        self.assertEqual(dict_list[0]['Publisher'], 'Nintendo')
-        self.assertEqual(dict_list[0]['NA_Sales'], 41.49)
-        self.assertEqual(dict_list[0]['EU_Sales'], 29.02)
-        self.assertEqual(dict_list[0]['JP_Sales'], 3.77)
-        self.assertEqual(dict_list[0]['Other_Sales'], 8.46)
-        self.assertEqual(dict_list[0]['Global_Sales'], 82.74)
+    def test_carga_csv_lista(self):
+        lista = carga_csv_lista('vgsales.csv')
+        self.assertEqual(len(lista), 16598)
+        self.assertEqual(lista[0]['Rank'], 1)
+        self.assertEqual(lista[0]['Name'], 'Wii Sports')
+        self.assertEqual(lista[0]['Platform'], 'Wii')
+        self.assertEqual(lista[0]['Year'], 2006)
+        self.assertEqual(lista[0]['Genre'], 'Sports')
+        self.assertEqual(lista[0]['Publisher'], 'Nintendo')
+        self.assertEqual(lista[0]['NA_Sales'], 41.49)
+        self.assertEqual(lista[0]['EU_Sales'], 29.02)
+        self.assertEqual(lista[0]['JP_Sales'], 3.77)
+        self.assertEqual(lista[0]['Other_Sales'], 8.46)
+        self.assertEqual(lista[0]['Global_Sales'], 82.74)
         
 
  
