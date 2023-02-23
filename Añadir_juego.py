819d0a10e0a3ab329107abb330ca105331132d3d
rafa <rafababla@hotmail.com>
2023-02-23 10:00:51 +0100

    añadir juego 2

--- Añadir_juego.py
+++ Añadir_juego.py
@@ -1,13 +1,18 @@
+import csv
+from convertir_csv_diccionario import *
+
+
 def obtener_valor(valor):
-    while not valor:
-        print("El valor no puede estar vacío.")
-        valor = input("Introduzca un valor: ")
-    return valor
+    if not valor:
+        return 'Desconocido'
+    else:
+        return valor
+
 
 def obtener_float(dato):
     while True:
         if not dato:
-            print("El valor no puede estar vacío.")
+            return 'Desconocido'
         else:
             try:
                 dato_float = float(dato)
@@ -16,36 +21,47 @@ def obtener_float(dato):
                 print("El valor debe ser un número.")
         dato = input("Ingrese el valor nuevamente: ")
 
+juegos=convertir_csv_diccionarios("vgsales.csv")
+
 def agregar_juego(juegos):
     encurso = True
-    
+
+    print('Si el campo se deja vacio, se pondra como desconocido ')
+
     while encurso:
-        nombre = input("Introduzca el nombre del juego: ")
-        obtener_valor(nombre)
-        plataforma = input("Introduzca la plataforma del juego: ")
-        obtener_valor(plataforma)
         
-        año = None
+        nombre = input("Indica el nombre del juego: ")
+        obtener_valor(nombre)
+        plataforma = input("Indica la plataforma del juego: ")
+        obtener_valor(plataforma)
+
+        año = None  # Inicializar variable aquí
+
         while año is None:
             try:
-                año = int(input("Introduzca el año de lanzamiento del juego: "))
+                año = int(input("Indica el año de lanzamiento del juego: "))
+                assert 1950 <= año <= 2024, "El año debe estar entre 1950 y 2024."
             except ValueError:
                 print("El año debe ser un número entero.")
-        genero = input("Introduzca el género del juego: ")
-        obtener_valor(genero)        
-        empresa = input("Introduzca la empresa del juego: ")
+            except AssertionError as error:
+                print(error)
+                año = None
+
+        genero = input("Indica el género del juego: ")
+        obtener_valor(genero)
+        empresa = input("Indica la empresa del juego: ")
         obtener_valor(empresa)
-        na_sales =  input('introduce valor de ventas de Norte America')
+        na_sales = input('Indica valor de ventas de Norte America')
         obtener_float(na_sales)
-        eu_sales = input('introduce valor de ventas de Europa')
-        obtener_float(eu_sales)    
-        jp_sales = input('introduce valor de ventas de Japon')
+        eu_sales = input('Indica valor de ventas de Europa')
+        obtener_float(eu_sales)
+        jp_sales = input('Indica valor de ventas de Japon')
         obtener_float(jp_sales)
-        other_sales = input('introduce valor de ventas del resto del mundo')
+        other_sales = input('Indica valor de ventas del resto del mundo')
         obtener_float(other_sales)
-        global_sales = input('introduce valor de ventas a nivel global')
+        global_sales = input('Indica valor de ventas a nivel global')
         obtener_float(global_sales)
-    
+
         juego = {
             "nombre": nombre,
             "plataforma": plataforma,
@@ -61,8 +77,9 @@ def agregar_juego(juegos):
 
         a = True
         while a:
-            confirmar= input("¿Quieres agregar este juego? (1 = si/2 = no):")
-            
+            confirmar = input(
+                f"¿Quieres agregar este juego? (1 = si/2 = no): {juego}")
+
             if confirmar == '1':
                 juegos.append(juego)
                 print("El juego se ha agregado correctamente.")
@@ -72,23 +89,21 @@ def agregar_juego(juegos):
                 a = False
             else:
                 print("Tiene que ser 1: Si o 2: No")
-        b= True
-        while encurso:
-            while b:
-                confirmar = input("¿Desea agregar otro juego? (1 = si/2 = no): ")
+        b = True
+        
+        while b:
+            confirmar = input(
+                "¿Desea agregar otro juego? (1 = si/2 = no): ")
+
+            if confirmar == '1':
+                encurso = True
+                b = False
+
+            elif confirmar == '2':
+                encurso = False
+                b = False
+            else:
+                confirmar = input(
+                        "¿Desea agregar otro juego? (1 = si/2 = no): ")
+                b = True
                 
-                if confirmar == '1':
-                    encurso = True
-                    b = False
-                
-                elif confirmar == '2':
-                    encurso = False
-                    b = False
-                else: 
-                    confirmar = input("¿Desea agregar otro juego? (1 = si/2 = no): ")
-                    b = True
-
-
-
-
-
