ccecc2aca5304fc4780b7eb5eb49def5ea9e9749
Veronica <veronicaarevaloarribas@gmail.com>
2023-02-23 12:06:07 +0100

    prueba calidad siglo 20

--- buscar_siglo_veinte.py
+++ buscar_siglo_veinte.py
@@ -4,9 +4,10 @@ import pandas as pd
 def buscar_siglo_veinte(archivo):
 
     df = pd.read_csv(archivo)
-    df= pd.read_csv(archivo)
-    df= df[df['Year'] < 2000]
+    df = pd.read_csv(archivo)
+    df = df[df['Year'] < 2000]
     print("Juegos del siglo XX:\n")
     print(df)
+
 
 buscar_siglo_veinte("vgsales.csv")
